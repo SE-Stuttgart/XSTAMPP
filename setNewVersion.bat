fb9c190deca15975e79cdaf204a7c1c4566ee841
Lukas Balzer <swt87885@stud.uni-stuttgart.de>
2017-05-29 20:24:18 +0200

    increased version to 2.3

--- setNewVersion.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Auf welche Version soll das Projekt gesetzt werden? 
-set /P Version=
-mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=%Version%
-
-pause
