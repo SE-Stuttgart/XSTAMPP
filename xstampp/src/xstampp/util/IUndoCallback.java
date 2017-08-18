891c853b891fda2f8ab29574ba0fd5e73fe5709a
Lukas Balzer <swt87885@stud.uni-stuttgart.de>
2017-08-18 10:03:48 +0200

    changed version of haz schema and documented IUndoCallback methods

--- xstampp/src/xstampp/util/IUndoCallback.java
+++ xstampp/src/xstampp/util/IUndoCallback.java
@@ -9,11 +9,33 @@
 package xstampp.util;
 
 import xstampp.model.ObserverValue;
+import xstampp.util.service.UndoRedoService;
 
+/**
+ * This interface is used in the {@link UndoRedoService} of the xstampp platform. it is used to fill
+ * a command stack in by pushing through the {@link UndoRedoService#push(IUndoCallback)} method.
+ *
+ */
 public interface IUndoCallback {
+
+  /**
+   * This method is called to undo all actions performed by the event that constructed this object.
+   * Note that during the call of undo in the {@link UndoRedoService} no new {@link IUndoCallback}
+   * is added to the stack.
+   */
   void undo();
 
+  /**
+   * This method is called to redo the actions performed by the event that constructed this object.
+   * Note that during the call of redo in the {@link UndoRedoService} no new {@link IUndoCallback}
+   * is added to the stack.
+   */
   void redo();
 
+  /**
+   * 
+   * @return the {@link ObserverValue} that can be used to notify Observers/Clients/... about
+   *         changes in the data model.
+   */
   ObserverValue getChangeConstant();
 }
