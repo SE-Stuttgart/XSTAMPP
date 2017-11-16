770fad0719a45c0eb46bd98424696dc40d5ce848
Lukas Balzer <balzer814_dev@web.de>
2017-11-16 15:33:32 +0100

    Adapt ACast Plugin to data model changes

--- xstampp.acast/src/acast/jobs/ExportJob.java
+++ xstampp.acast/src/acast/jobs/ExportJob.java
@@ -1,15 +1,9 @@
 package acast.jobs;
 
-import java.net.URL;
 import java.util.UUID;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.URIResolver;
-import javax.xml.transform.stream.StreamSource;
 
 import acast.controller.Controller;
 import xstampp.ui.common.ProjectManager;
@@ -24,71 +18,46 @@ import xstampp.util.ExportPackage;
  */
 public class ExportJob extends xstampp.astpa.util.jobs.ExportJob {
 
-	/**
-	 * Constructor of the export job
-	 *
-	 * @author Fabian Toth, Lukas Balzer
-	 * @param projectId
-	 *            the project which
-	 * @param name
-	 *            the name of the job
-	 * @param filePath
-	 *            the path to the pdf file
-	 * @param xslName
-	 *            the name of the file in which the xsl file is stored which should
-	 *            be used
-	 * @param asOne
-	 *            true if all content shall be exported on a single page
-	 * @param decorate
-	 *            if the control structure components should be pictured with
-	 *            colored borders and image labels
-	 */
-	public ExportJob(UUID projectId, ExportPackage data, boolean asOne, boolean decorate) {
-		super(projectId, data, asOne, decorate);
-	}
+  /**
+   * Constructor of the export job
+   *
+   * @author Fabian Toth, Lukas Balzer
+   * @param projectId
+   *          the project which
+   * @param name
+   *          the name of the job
+   * @param filePath
+   *          the path to the pdf file
+   * @param xslName
+   *          the name of the file in which the xsl file is stored which should
+   *          be used
+   * @param asOne
+   *          true if all content shall be exported on a single page
+   * @param decorate
+   *          if the control structure components should be pictured with
+   *          colored borders and image labels
+   */
+  public ExportJob(UUID projectId, ExportPackage data, boolean asOne, boolean decorate) {
+    super(projectId, data, asOne, decorate);
+  }
 
-	@Override
-	protected boolean canExport() {
-		return false;
-	}
+  @Override
+  protected boolean canExport() {
+    return false;
+  }
 
-	@Override
-	protected Object getModel() {
-		return ProjectManager.getContainerInstance().getDataModel(getProjectId());
-	}
+  @Override
+  protected Object getModel() {
+    return ProjectManager.getContainerInstance().getDataModel(getProjectId());
+  }
 
-	@Override
-	protected JAXBContext getModelContent() throws JAXBException {
-		return JAXBContext.newInstance(Controller.class);
-	}
+  @Override
+  protected JAXBContext getModelContent() throws JAXBException {
+    return JAXBContext.newInstance(Controller.class);
+  }
 
-	/**
-	 * @deprecated Use {@link #getxslTransformer(String,ClassLoader)} instead
-	 */
-	@Override
-	protected Transformer getxslTransformer(String resource) {
-		return getxslTransformer(resource, null);
-	}
-
-	@Override
-	protected Transformer getxslTransformer(String resource, Class loader) {
-		URL xslUrl = this.getClass().getResource(resource);
-
-		if (xslUrl == null) {
-			return null;
-		}
-		try {
-			StreamSource transformXSLSource = new StreamSource(xslUrl.openStream());
-			TransformerFactory transfact = TransformerFactory.newInstance();
-			transfact.setURIResolver(new URIResolver() {
-				@Override
-				public Source resolve(String href, String base) {
-					return new StreamSource(this.getClass().getClassLoader().getResourceAsStream("/" + href)); //$NON-NLS-1$
-				}
-			});
-			return transfact.newTransformer(transformXSLSource);
-		} catch (Exception e) {
-			return null;
-		}
-	}
+  @Override
+  protected Class<?> getResourceLoader() {
+    return getClass();
+  }
 }
