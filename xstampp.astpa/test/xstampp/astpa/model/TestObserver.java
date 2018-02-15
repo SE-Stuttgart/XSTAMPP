611296c9bd2f00b62dba91f5abec9c529c5fca5e
Lukas Balzer <balzer814_dev@web.de>
2018-02-15 13:21:15 +0100

    Add copyright headers to xstampp files

--- xstampp.astpa/test/xstampp/astpa/model/TestObserver.java
+++ xstampp.astpa/test/xstampp/astpa/model/TestObserver.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (C) 2018 Lukas Balzer, Asim Abdulkhaleq, Stefan Wagner Institute of SoftwareTechnology, Software Engineering Group University of Stuttgart, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Lukas Balzer, Asim Abdulkhaleq, Stefan Wagner Institute of SoftwareTechnology, Software Engineering Group University of Stuttgart, Germany - initial API and implementation
+ ******************************************************************************/
 package xstampp.astpa.model;
 
 import java.util.ArrayList;
