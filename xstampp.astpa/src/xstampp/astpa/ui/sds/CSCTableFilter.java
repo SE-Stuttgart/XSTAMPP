3b2f8fcc110f6a81613733849f273189269860f6
Lukas Balzer <balzer814_dev@web.de>
2018-07-11 11:08:56 +0200

    Fix NullPointer when using the CorrespondingSafetyConstraints Filter on empty safety constraints

--- xstampp.astpa/src/xstampp/astpa/ui/sds/CSCTableFilter.java
+++ xstampp.astpa/src/xstampp/astpa/ui/sds/CSCTableFilter.java
@@ -1,12 +1,14 @@
 /*******************************************************************************
- * Copyright (C) 2018 Lukas Balzer, Asim Abdulkhaleq, Stefan Wagner Institute of SoftwareTechnology, Software Engineering Group University of Stuttgart, Germany.
+ * Copyright (C) 2018 Lukas Balzer, Asim Abdulkhaleq, Stefan Wagner Institute of SoftwareTechnology,
+ * Software Engineering Group University of Stuttgart, Germany.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *     Lukas Balzer, Asim Abdulkhaleq, Stefan Wagner Institute of SoftwareTechnology, Software Engineering Group University of Stuttgart, Germany - initial API and implementation
+ * Lukas Balzer, Asim Abdulkhaleq, Stefan Wagner Institute of SoftwareTechnology, Software
+ * Engineering Group University of Stuttgart, Germany - initial API and implementation
  ******************************************************************************/
 package xstampp.astpa.ui.sds;
 
@@ -20,40 +22,47 @@ import xstampp.ui.common.ProjectManager;
 
 public class CSCTableFilter extends ModeFilter {
 
-    public CSCTableFilter() {
-      super( new String[] { Messages.All,
-          Messages.CorrespondingSafetyConstraints, Messages.UnsafeControlActions });
-    }
+  public CSCTableFilter() {
+    super(new String[] { Messages.All,
+        Messages.CorrespondingSafetyConstraints, Messages.UnsafeControlActions });
+  }
 
-    @Override
-    public boolean select(Viewer viewer, Object parentElement, Object element) {
-      if ((this.searchString != null) && (this.searchString.length() != 0)) {
-        ICorrespondingUnsafeControlAction cuca = (ICorrespondingUnsafeControlAction) element;
-        try {
-          switch (this.getCSCFilterMode()) {
-          case 1:
-            return checkCSCText(cuca);
-          case 2:
-            return checkUcaDescription(cuca);
-          case 0:
-            return checkCSCText(cuca) || checkUcaDescription(cuca);
-          }
-        } catch (PatternSyntaxException exc) {
-          ProjectManager.getLOGGER().debug("Exception in the CSC Table filter" + exc.getMessage());
+  @Override
+  public boolean select(Viewer viewer, Object parentElement, Object element) {
+    if ((this.searchString != null) && (this.searchString.length() != 0)) {
+      ICorrespondingUnsafeControlAction cuca = (ICorrespondingUnsafeControlAction) element;
+      try {
+        switch (this.getCSCFilterMode()) {
+        case 1:
+          return checkCSCText(cuca);
+        case 2:
+          return checkUcaDescription(cuca);
+        case 0:
+          return checkCSCText(cuca) || checkUcaDescription(cuca);
         }
-        return false;
+      } catch (PatternSyntaxException exc) {
+        ProjectManager.getLOGGER().debug("Exception in the CSC Table filter" + exc.getMessage());
       }
-      return true;
+      return false;
     }
+    return true;
+  }
 
-    private boolean checkCSCText(ICorrespondingUnsafeControlAction cuca) throws PatternSyntaxException{
+  private boolean checkCSCText(ICorrespondingUnsafeControlAction cuca) throws PatternSyntaxException {
+    try {
       return cuca.getCorrespondingSafetyConstraint().getText().toLowerCase()
           .matches(this.searchString);
+    } catch (NullPointerException exc) {
+      return false;
     }
-
-    private boolean checkUcaDescription(ICorrespondingUnsafeControlAction cuca) throws PatternSyntaxException{
-      return cuca.getDescription().toLowerCase().matches(this.searchString);
-    }
-
-
   }
+
+  private boolean checkUcaDescription(ICorrespondingUnsafeControlAction cuca) throws PatternSyntaxException {
+    try {
+      return cuca.getDescription().toLowerCase().matches(this.searchString);
+    } catch (NullPointerException exc) {
+      return false;
+    }
+  }
+
+}
